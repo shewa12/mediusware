3074b4f8ba33e45373b5cf1984aa44e5719a32a3
MhShohel <mhshohel89@gmail.com>
2019-03-28 12:26:06 +0600

    Fixed course prerequisites, deletd edd and woocommerce from pro

--- addons/tutor-prerequisites/views/frontend/show-course-prerequisites.php
+++ addons/tutor-prerequisites/views/frontend/show-course-prerequisites.php
@@ -7,7 +7,7 @@
 
 
 <div class="course-prerequisites-lists-wrap">
-	<ul class="prerequisites-course-lists">
+    <ul class="prerequisites-course-lists">
 		<?php
 		$savedPrerequisitesIDS = maybe_unserialize(get_post_meta(get_the_ID(), '_tutor_course_prerequisites_ids', true));
 		if (is_array($savedPrerequisitesIDS) && count($savedPrerequisitesIDS)){
@@ -15,13 +15,13 @@
 				?>
                 <li>
                     <a href="<?php echo get_the_permalink($courseID); ?>" class="prerequisites-course-item">
-					<span class="prerequisites-course-feature-image">
-						<?php echo get_the_post_thumbnail($courseID); ?>
-					</span>
+                        <span class="prerequisites-course-feature-image">
+                            <?php echo get_the_post_thumbnail($courseID); ?>
+                        </span>
 
                         <span class="prerequisites-course-title">
-						<?php echo get_the_title($courseID); ?>
-					</span>
+                            <?php echo get_the_title($courseID); ?>
+                        </span>
 
 						<?php if (tutor_utils()->is_completed_course($courseID)){
 							?>
@@ -32,8 +32,8 @@
                 </li>
 				<?php
 			}
-        }
+		}
 
 		?>
-	</ul>
+    </ul>
 </div>
