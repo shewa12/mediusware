42200f88d7256f6535012b7631d0ff13b648719a
MhShohel <mhshohel89@gmail.com>
2019-06-21 17:59:49 +0600

    Added Assignments addons

---
+++ addons/tutor-assignments/classes/Assignments_List.php
@@ -0,0 +1,147 @@
+<?php
+namespace TUTOR_ASSIGNMENTS;
+
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+if (! class_exists('Tutor_List_Table')){
+	include_once tutor()->path.'classes/Tutor_List_Table.php';
+}
+
+class Assignments_List extends \Tutor_List_Table {
+
+	function __construct(){
+		global $status, $page;
+
+		//Set parent defaults
+		parent::__construct( array(
+			'singular'  => 'assignment',     //singular name of the listed records
+			'plural'    => 'assignments',    //plural name of the listed records
+			'ajax'      => false        //does this table support ajax?
+		) );
+	}
+
+	function column_default($item, $column_name){
+		switch($column_name){
+			case 'user_email':
+				return $item->$column_name;
+			default:
+				return print_r($item,true); //Show the whole array for troubleshooting purposes
+		}
+	}
+
+	function column_mark($item){
+		echo tutor_utils()->get_assignment_option($item->comment_post_ID, 'total_mark');
+	}
+	function column_passing_mark($item){
+		echo tutor_utils()->get_assignment_option($item->comment_post_ID, 'pass_mark');
+	}
+	function column_student($item){
+		echo $item->comment_author;
+	}
+	function column_title($item){
+		echo '<a href="'.get_the_permalink($item->comment_post_ID).'" target="_blank">'.get_the_title($item->comment_post_ID).'</a>';
+	}
+	function column_duration($item){
+		echo tutor_utils()->get_assignment_option($item->comment_post_ID, 'time_duration.value').' '.tutor_utils()->get_assignment_option
+			($item->comment_post_ID, 'time_duration.time');
+	}
+	function column_date($item){
+		echo '<p>';
+		echo __('Started', 'tutor-pro').' : ';
+		echo date(get_option('date_format').' '.get_option('time_format'), strtotime($item->comment_date_gmt)); //Submit Finished
+		echo '</p>';
+
+		echo '<p>';
+		echo __('Finished', 'tutor-pro').' : ';
+		echo date(get_option('date_format').' '.get_option('time_format'), strtotime($item->comment_date)); //Submit Finished
+		echo '</p>';
+	}
+
+	function column_action($item){
+		echo "<a href='".admin_url('admin.php?page=tutor-assignments&view_assignment='.$item->comment_ID)."'>".__('View', 'tutor-pro')."</a>";
+	}
+
+	/**
+	 * @param $item
+	 *
+	 * Completed Course by User
+	 */
+	function column_course($item){
+		echo '<a href="'.get_the_permalink($item->comment_parent).'" target="_blank">'.get_the_title($item->comment_parent).'</a>';
+	}
+
+	function column_cb($item){
+		return sprintf(
+			'<input type="checkbox" name="%1$s[]" value="%2$s" />',
+			/*$1%s*/ $this->_args['singular'],  //Let's simply repurpose the table's singular label ("assignment")
+			/*$2%s*/ $item->comment_ID                //The value of the checkbox should be the record's id
+		);
+	}
+
+	function get_columns(){
+		$columns = array(
+			'cb'                => '<input type="checkbox" />', //Render a checkbox instead of text
+			'title'      => __('Title', 'tutor'),
+			'student'        => __('Student', 'tutor'),
+			'course'  => __('Course', 'tutor'),
+			'mark'  => __('Mark', 'tutor'),
+			'passing_mark'  => __('Passing Mark', 'tutor'),
+			'duration'  => __('Duration', 'tutor'),
+			'date'  => __('Date', 'tutor'),
+			'action'  => __('Actions', 'tutor'),
+		);
+		return $columns;
+	}
+
+	function prepare_items() {
+		global $wpdb;
+
+		$per_page = 20;
+
+		$search_term = '';
+		if (isset($_REQUEST['s'])){
+			$search_term = sanitize_text_field($_REQUEST['s']);
+		}
+
+		$columns = $this->get_columns();
+		$hidden = array();
+		$sortable = $this->get_sortable_columns();
+
+		$this->_column_headers = array($columns, $hidden, $sortable);
+		//$this->process_bulk_action();
+
+		$current_page = $this->get_pagenum();
+
+		$total_items = 0;
+
+		$in_courses_ids = '';
+		if ( ! current_user_can('administrator') && current_user_can(tutor()->instructor_role)) {
+			$courses_ids = tutor_utils()->get_assigned_courses_ids_by_instructors();
+			if (tutor_utils()->count($courses_ids)){
+				$in_courses_ids = "'".implode("','", $courses_ids)."'";
+
+				$total_items = $wpdb->get_var("SELECT COUNT(comment_ID) FROM {$wpdb->comments} WHERE comment_type = 'tutor_assignment' AND comment_approved = 'submitted' AND comment_parent IN({$in_courses_ids})   ");
+			}
+		}else{
+			$total_items = $wpdb->get_var("SELECT COUNT(comment_ID) FROM {$wpdb->comments} WHERE comment_type = 'tutor_assignment' AND comment_approved = 'submitted'    ");
+		}
+
+		$assignment_items = array();
+		$start = ($current_page-1)*$per_page;
+
+		if ( ! current_user_can('administrator') && current_user_can(tutor()->instructor_role)) {
+			$assignment_items = $wpdb->get_results("SELECT * FROM {$wpdb->comments} WHERE comment_type = 'tutor_assignment' AND comment_approved = 'submitted'  AND comment_parent IN({$in_courses_ids}) LIMIT {$start}, {$per_page} ");
+		}else{
+			$assignment_items = $wpdb->get_results("SELECT * FROM {$wpdb->comments} WHERE comment_type = 'tutor_assignment' AND comment_approved = 'submitted' LIMIT {$start}, {$per_page} ");
+		}
+
+		$this->items = $assignment_items;
+
+		$this->set_pagination_args( array(
+			'total_items' => $total_items,                  //WE have to calculate the total number of items
+			'per_page'    => $per_page,                     //WE have to determine how many items to show on a page
+			'total_pages' => ceil($total_items/$per_page)   //WE have to calculate the total number of pages
+		) );
+	}
+}
--- tutor-pro.php
+++ tutor-pro.php
@@ -4,10 +4,10 @@ Plugin Name: Tutor LMS Pro
 Plugin URI: https://www.themeum.com/product/tutor-lms/
 Description: Power up Tutor LMS plugins by Tutor Pro
 Author: Themeum
-Version: 1.3.2
+Version: 1.3.3
 Author URI: http://themeum.com
 Requires at least: 4.5
-Tested up to: 5.1
+Tested up to: 5.2
 Text Domain: tutor-pro
 Domain Path: /languages/
 */
@@ -17,10 +17,10 @@ if ( ! defined( 'ABSPATH' ) )
 /**
  * Defined the tutor main file
  */
-define('TUTOR_PRO_VERSION', '1.3.2');
+define('TUTOR_PRO_VERSION', '1.3.3');
 define('TUTOR_PRO_FILE', __FILE__);
 
-if ( ! function_exists('tutor')) {
+if ( ! function_exists('tutor_pro')) {
 	function tutor_pro() {
 		$path = plugin_dir_path( TUTOR_PRO_FILE );
 		$info = array(
