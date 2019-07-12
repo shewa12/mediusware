fc11b11a4d9acd695312d373b38b86fbfdc05023
Delowar Hossain <delowar@Delowars-Mac-mini.local>
2019-07-12 17:39:17 +0600

    attachment metabox markup updated

--- addons/tutor-course-attachments/views/metabox/course-attachments-metabox.php
+++ addons/tutor-course-attachments/views/metabox/course-attachments-metabox.php
@@ -1,23 +1,21 @@
 <div class="tutor-lesson-attachments-metabox">
 
-	<div class="tutor-added-attachments-wrap">
+	<div class="tutor-added-attachments-wrap tutor-course-builder-attachements">
 		<?php
-		$attachments = tutor_utils()->get_attachments();
-		if ( is_array($attachments) && count($attachments)) {
-			foreach ( $attachments as $attachment ) {
-				?>
-				<div class="tutor-added-attachment">
-					<p><a href="javascript:;" class="tutor-delete-attachment">&times;</a>
-						<span>
-							<a href="<?php echo $attachment->url; ?>"><?php echo $attachment->name; ?></a>
-						</span>
-					</p>
-					<input type="hidden" name="tutor_attachments[]" value="<?php echo $attachment->id; ?>">
-				</div>
-			<?php }
-		}
+            $attachments = tutor_utils()->get_attachments();
+            if ( is_array($attachments) && count($attachments)) {
+                foreach ( $attachments as $attachment ) {
+                    ?>
+                    <div class="tutor-added-attachment">
+                        <i class="tutor-icon-archive"></i>
+                            <a href="javascript:;" class="tutor-delete-attachment">&times;</a>
+                            <span><a href="<?php echo $attachment->url; ?>"><?php echo $attachment->name; ?></a></span>
+                        <input type="hidden" name="tutor_attachments[]" value="<?php echo $attachment->id; ?>">
+                    </div>
+                <?php }
+            }
 		?>
 	</div>
 
-	<button type="button" class="tutor-course-builder-button active tutorUploadAttachmentBtn"><?php _e('Upload Attachment', 'tutor'); ?></button>
+	<button type="button" class="tutor-course-builder-button tutor-btn-lg active tutorUploadAttachmentBtn"><i class="tutor-icon-attach"></i><?php _e('Upload Attachment', 'tutor'); ?></button>
 </div>
