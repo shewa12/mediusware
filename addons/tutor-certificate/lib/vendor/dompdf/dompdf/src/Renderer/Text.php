72b0c4e701a5203a75e6b3508e7cbc295b5bf4d5
Mh Shohel <mhshohel89@gmail.com>
2020-04-16 17:01:31 +0600

    * Improved: Certificate generator PDF compatibility with PHP 7.3

--- addons/tutor-certificate/lib/vendor/dompdf/dompdf/src/Renderer/Text.php
+++ addons/tutor-certificate/lib/vendor/dompdf/dompdf/src/Renderer/Text.php
@@ -137,7 +137,7 @@ class Text extends AbstractRenderer
 
             switch ($text_deco) {
                 default:
-                    continue;
+                    break;
 
                 case "underline":
                     $deco_y += $base - $descent + $underline_offset + $line_thickness / 2;
