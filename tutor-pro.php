820346a9543f011c66362293ae679f8b15932397
Mh Shohel <mhshohel89@gmail.com>
2020-06-29 12:31:48 +0600

    merging with saju

--- tutor-pro.php
+++ tutor-pro.php
@@ -4,7 +4,7 @@ Plugin Name: Tutor LMS Pro
 Plugin URI: https://www.themeum.com/product/tutor-lms/
 Description: Power up Tutor LMS plugins by Tutor Pro
 Author: Themeum
-Version: 1.6.2
+Version: 1.6.4
 Author URI: http://themeum.com
 Requires at least: 4.5
 Tested up to: 5.4
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) )
 /**
  * Defined the tutor main file
  */
-define('TUTOR_PRO_VERSION', '1.6.2');
+define('TUTOR_PRO_VERSION', '1.6.4');
 define('TUTOR_PRO_FILE', __FILE__);
 
 /**
