5cce699d3f5a17c4ec6c80c47e2a7e7ed5d0e697
JK <jayedulk@gmail.com>
2020-08-12 14:51:40 +0600

    Coding Style Fixed

--- addons/tutor-certificate/classes/HTML_to_Image.php
+++ addons/tutor-certificate/classes/HTML_to_Image.php
@@ -6,17 +6,13 @@
 
 namespace TUTOR_CERT;
 
-class HTML_to_Image
-{
-    public function register_script()
-    {
-        add_action('wp_enqueue_scripts', [$this, 'load_script']);
+class HTML_to_Image{
+    public function register_script(){
+        add_action('wp_enqueue_scripts', array($this, 'load_script'));
     }
 
-    public function load_script()
-    {
-        if(is_single_course() || !empty($_GET['cert_hash']))
-        {
+    public function load_script(){
+        if(is_single_course() || !empty($_GET['cert_hash'])){
             $base = tutor_pro()->url.'addons/tutor-certificate/assets/js/';
             
             wp_enqueue_script('html-to-image-converter', $base.'html2canvas.min.js');
